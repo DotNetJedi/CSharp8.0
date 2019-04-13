2bcff64897bdbdcaa1fb2170065cb6381f588838
Eric De Carufel <eric.decarufel@gmail.com>
2019-04-13 13:58:24 -0400

    Add project files.

---
+++ CSharp8.0/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace CSharp8._0
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var subscribers = Service.GetSubscribers();
+            var names = GetNames(subscribers);
+
+            foreach (var name in names)
+            {
+                Console.WriteLine($"{name} has subscribed to the service");
+            }
+        }
+
+        static IEnumerable<string> GetNames(IEnumerable<Person> people)
+        {
+            foreach (var p in people)
+            {
+                yield return GetName(p);
+            }
+        }
+
+        static string GetName(Person p)
+        {
+            return $"{p.FirstName} {p.MiddleName[0]}. {p.LastName}";
+        }
+    }
+
+    public class Person
+    {
+        public string FirstName { get; set; }
+        public string MiddleName { get; set; }
+        public string LastName { get; set; }
+
+        public Person(string firstName, string middleName, string lastName)
+        {
+            FirstName = firstName;
+            MiddleName = middleName;
+            LastName = lastName;
+        }
+    }
+}
---
+++ CSharp8.0/Service.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CSharp8._0
+{
+    class Service
+    {
+        public static IEnumerable<Person> GetSubscribers()
+        {
+            yield return new Person("Tracey", "Jaron", "Downer");
+            yield return new Person("Thorburn", "Wat", "Tuft");
+            yield return new Person("Marisa", "Sukie", "Waters");
+            yield return new Person("Rosy", "Christy", "Bannister");
+            yield return new Person("Dominick", "Maximillian", "Ayers");
+        }
+    }
+}
